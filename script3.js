3ef097ea1197d88b6e07d86314e68767fab41ac6
Anujjjjj <90022664+Anujjjjj@users.noreply.github.com>
2021-11-01 18:37:09 +0530

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,5 @@
+var x="500";
+var y;
+console.log("here the defined value of x is:" +x);
+console.log("here no value of y is given        " +y);
+console.log("we had not assigned a variable with name anuj" +anuj);
