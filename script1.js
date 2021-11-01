cead464595562edea175bc07025ef6283a1f227f
Anujjjjj <90022664+Anujjjjj@users.noreply.github.com>
2021-11-01 18:31:35 +0530

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,3 @@
+var x= "hello . how are you??";
+var y= " yeah you did all steps correctly . this is that page you were looking for. "
+var z= "this is that linked js files's output . these all you are geeting because of that js file only."
