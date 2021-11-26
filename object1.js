46de98b30efac1556ab813cb216465c601632b9f
Anujjjjj <90022664+Anujjjjj@users.noreply.github.com>
2021-11-26 12:48:44 +0530

    Create object1.js

---
+++ object1.js
@@ -0,0 +1,26 @@
+var person1 = {
+	firstname: "Anuj",
+ lastname: "Kumar Singh",
+ adress: "gopalganj,bihar,841428.", 
+};
+console.log(person1.firstname);
+console.log(person1.lastname);
+console.log(person1.adress);
+var person2 = {
+	firstname:"gollamandala",
+	lastname: "gopi",
+	adress: "vijayawada,andhra pradesh,502355.",
+};
+console.log(person2.firstname);
+console.log(person2.lastname);
+console.log(person2.adress);
+
+
+var person3= {
+	firstname: "Aryamaan",
+	lastname: "kumar" ,
+	address: "patna, bihar,180001.",
+};
+console.log(person3.firstname);
+console.log(person3.lastname);
+console.log(person3.address);
